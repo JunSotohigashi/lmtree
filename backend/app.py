edf252af52d4457daf1b9a77c1b3d7773667f4e6
JunSotohigashi <c6h4ohcooc6h5@gmail.com>
2024-12-30 21:03:49 +0900

    Install Gunicorn for WSGI server

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+counter = 0
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    global counter
+    counter += 1
+    return f"<p>Hello, World! <br /> Count: {counter}</p>"
